b014dc662cd8cc20a315f799c782201f1e15013e
billdev1958 <billdev1958@gmail.com>
2024-12-08 20:47:47 -0600

    json

--- alumnos/models/alumno.go
+++ alumnos/models/alumno.go
@@ -14,14 +14,14 @@ type Alumno struct {
 }
 
 type RegisterAlumnRequest struct {
-	Name            string
-	Lastname1       string
-	Lastname2       string
-	CourseID        int
-	CurrentCourseID int
-	Subjects        []SubjectID
+	Name            string      `json:"name"`
+	Lastname1       string      `json:"lastname1"`
+	Lastname2       string      `json:"lastname2"`
+	CourseID        int         `json:"course_id"`
+	CurrentCourseID int         `json:"current_course_id"`
+	Subjects        []SubjectID `json:"subjects"`
 }
 
 type SubjectID struct {
-	ID int
+	ID int `json:"id"`
 }
